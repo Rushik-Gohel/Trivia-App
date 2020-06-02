555f7d9a5d3cb5e833f0da7e1d00266ae99463ac
Rushik Gohel <gohel.rushik.btech2018@sitpune.edu.in>
2020-06-02 11:17:45 +0530

    Initial commit

---
+++ app/src/main/java/com/example/trivia/MainActivity.java
@@ -0,0 +1,169 @@
+package com.example.trivia;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.trivia.controller.AppController;
+import com.example.trivia.data.AnswerListAsyncResponse;
+import com.example.trivia.data.QuestionBank;
+import com.example.trivia.model.Question;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView questionTextview;
+    private TextView questionCounterTextview;
+    private Button trueButton;
+    private Button falseButton;
+    private ImageButton prevButton;
+    private ImageButton nextButton;
+    private int currentQuestionIndex = 0;
+    private List<Question> questionList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        nextButton = findViewById(R.id.next_button);
+        prevButton = findViewById(R.id.prev_button);
+        trueButton = findViewById(R.id.true_button);
+        falseButton = findViewById(R.id.false_button);
+        questionCounterTextview = findViewById(R.id.counter_text);
+        questionTextview = findViewById(R.id.question_textview);
+
+        prevButton.setOnClickListener(this);
+        nextButton.setOnClickListener(this);
+        trueButton.setOnClickListener(this);
+        falseButton.setOnClickListener(this);
+
+
+        questionList = new QuestionBank().getQuestions(new AnswerListAsyncResponse() {
+            @Override
+            public void processFinished(ArrayList<Question> questionArrayList) {
+
+                questionTextview.setText(questionArrayList.get(currentQuestionIndex).getAnswer());
+                questionCounterTextview.setText(currentQuestionIndex+" /"+ questionArrayList.size());
+                Log.d("Inside", "processFinished: "+ questionArrayList);
+
+            }
+        });
+        //Log.d("Main","onCreate: " + questionList);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.prev_button:
+                if(currentQuestionIndex>0){
+                    currentQuestionIndex = (currentQuestionIndex - 1) % questionList.size();
+                    updateQuestion();
+                }
+                break;
+            case R.id.next_button:
+                currentQuestionIndex = (currentQuestionIndex + 1) % questionList.size();
+                updateQuestion();
+                break;
+            case R.id.true_button:
+                checkAnswer(true);
+                updateQuestion();
+                break;
+            case R.id.false_button:
+                checkAnswer(false);
+                updateQuestion();
+                break;
+
+        }
+    }
+
+    private void checkAnswer(boolean userChooseCorrect) {
+        boolean answerIsTrue = questionList.get(currentQuestionIndex).isAnswerTrue();
+        int toastMessageId = 0;
+        if (userChooseCorrect == answerIsTrue){
+            fadeView();
+            toastMessageId = R.string.correct_answer;
+        }
+        else{
+            shakeAnimation();
+            toastMessageId = R.string.wrong_answer;
+        }
+        Toast.makeText(MainActivity.this,toastMessageId,
+                Toast.LENGTH_SHORT)
+                .show();
+    }
+
+    private void updateQuestion() {
+        String question = questionList.get(currentQuestionIndex).getAnswer();
+        questionTextview.setText(question);
+        questionCounterTextview.setText(currentQuestionIndex+" /"+ questionList.size());
+    }
+
+    private void fadeView(){
+        final CardView cardView = findViewById(R.id.cardView);
+        AlphaAnimation alphaAnimation = new AlphaAnimation(1.0f, 0.0f);
+
+        alphaAnimation.setDuration(350);
+        alphaAnimation.setRepeatCount(1);
+        alphaAnimation.setRepeatMode(Animation.REVERSE);
+
+        cardView.setAnimation(alphaAnimation);
+
+        alphaAnimation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+                cardView.setCardBackgroundColor(Color.GREEN);
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                cardView.setCardBackgroundColor(Color.WHITE);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+    }
+
+    private  void shakeAnimation(){
+        Animation shake = AnimationUtils.loadAnimation(MainActivity.this,
+                R.anim.shake_animation);
+        final CardView cardView = findViewById(R.id.cardView);
+        cardView.setAnimation(shake);
+
+        shake.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+                cardView.setCardBackgroundColor(Color.RED);
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                cardView.setCardBackgroundColor(Color.WHITE);
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+    }
+}
